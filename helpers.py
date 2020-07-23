ebef87643c4dfab6ba0e4570e5e4f851ec305f13
Amr Ibrahim Khudair <amrikhudair@outlook.com>
2020-07-23 21:13:35 +0200

    Add Fillable mixin to handle filling models from request form inputs

--- helpers.py
+++ helpers.py
@@ -1,2 +1,8 @@
 def csv(values, strip=True):
     return [v for v in ((v.strip() if strip else v) for v in values.split(',')) if v]
+
+class Fillable:
+    def fill(self, _except=[], **kwargs):
+        for key in kwargs:
+            if key not in _except and hasattr(self, key):
+                setattr(self, key, kwargs[key])
